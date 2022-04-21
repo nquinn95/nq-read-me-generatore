9404f06d296735a9c0ef606fa140125482b54d8d
nick quinn <nick.quinn@stadium1.com>
2022-04-21 17:52:58 -0400

    Fixed file pathing

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // TODO: Include packages needed for this application
-inquirer = require("requirer");
-fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path")
+const generateMarkdown = require("./utils/generateMarkdown");
+const { title } = require("process");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+//what is your gituhub unsername
+//what is your email address
+// what is your project title
+// please write a short project description
+// what kind of license does your project have
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
