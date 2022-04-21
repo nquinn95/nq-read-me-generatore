7de5fe999a2ee8b4d78cfe5da73e9fb2af8929bf
nick quinn <nick.quinn@stadium1.com>
2022-04-21 19:30:34 -0400

    created all the questions and the prompts with inquirer

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { title } = require("process");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your github username?", "What is your email address", "What is your project title", "Please write a short project description:", "What kind of license does your project have?"];
 
 //what is your gituhub unsername
 //what is your email address
@@ -15,6 +15,41 @@ const questions = [];
 // please write a short project description
 // what kind of license does your project have
 
+//prompt for the user to answer questions above
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "githubUsername",
+            message: questions[0]
+        },
+        {
+            type: "input",
+            name: "emailAddress",
+            message: questions[1]
+        },
+        {
+            type: "input",
+            name: "projectTitle",
+            message: questions[2]
+        },
+        {
+            type: "input",
+            name: "desc",
+            message: questions[3]
+        },
+        {
+            type: "input",
+            name: "license",
+            message: questions [4] + `\n MIT \n Apache \n defaults to none if there is no license `,
+            default: "None"
+        }
+
+    ])
+    .then((answers) => {
+        console.log(answers);
+        console.log(answers.license);
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
