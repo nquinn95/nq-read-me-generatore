c7743b0257046ced553c02bfb0a10c82f4854571
nick quinn <nick.quinn@stadium1.com>
2022-04-21 17:47:37 -0400

    added things to the gitignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // TODO: Include packages needed for this application
 inquirer = require("requirer");
-fs = require("fs"):
+fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
